c2ab4d4a5f29a35d98cdf596914c0ab48700b26a
Mariia Tereshchenko <108633009+Mlisovskaja@users.noreply.github.com>
2023-01-26 23:37:59 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
     }
   }
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
     if (prevState.contacts.length !== contacts.length) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
